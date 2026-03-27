977448f0b16e717297be8ee8fd2ac52f6cdc6003
arkforest <152924988+arkf0rest@users.noreply.github.com>
2026-03-27 09:41:10 +0100

    Delete BatailleNavale_NSI-main directory

--- BatailleNavale_NSI-main/Bataille_Navale/web/connection/connection.js
+++
@@ -1,7 +0,0 @@
-const pseudo = document.getElementById("pseudo").value;
-const login_btn = document.getElementById("login");
-
-login_btn.addEventListener("click", () => {
-            console.log(pseudo);
-            
-        });
--- BatailleNavale_NSI-main/README.md
+++
@@ -1,2 +0,0 @@
-# BatailleNavale_NSI
-https://prod.liveshare.vsengsaas.visualstudio.com/join?9C5C76094BE86A2001C0BFE4A3C20C791672
